bb6a99e4c954e2dd28a53280d6ed513301a6587d
tridoan <tridoan1998@gmail.com>
2021-10-27 01:07:13 -0700

    new code Thu Oct 27

--- daily_coding_probkem.py
+++ daily_coding_probkem.py
@@ -1,74 +1,10 @@
-
-#linked list , infinate,
-"""
-store integer,
-deleteme() => take Null pointer => delete that value from the linkedlist
-
-linkedlis: val, next, previous
-
-class linkedlist:
-    self __init__(self, val, next):
-        self.val = val
-        self.next = next
-7 => 5 => 4 => 3 ...
-     ^
-   node
-   node.next = 4
-   node.val = 5
-
-   node.val = node.next
-7 => 4 => 4 => 3 ...
-     ^
-     node
-     node.next = 4
-     node.next.next.val = 3
-     node = node.next.next
-7 => 4 => 3  ...
-
-
-def deleteme(node):
-    #copy the 4 to the 5
-    node.val = node.next.val
-    #7 => 4 => 4 => 3 ...
-
-    #delete the old 4
-    node.next = node.next.next
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-val: 7
-next: 5
-
-
-temp pointer point to 7
-run a loop iterate through the linkedlist
-temp = temp.next
-if temp.val == value:
-    #remove
-else:
-    continue
-
-
-
-
-
-
-7 => 4 => 3
-"""
-
-def deleteme():
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        tempsum, arr = 0, []
+        for i in range(len(nums) - 2):
+            for j in range(i + 1, len(nums) - 1):
+                for k in range(i + 2, len(nums)):
+                    tempsum = nums[i] + nums[j] + nums[k]
+                    if tempsum == 0:
+                        arr.append([nums[i], nums[j], nums[k]])
+        return arr
--- day9/send_mail.py
+++ day9/send_mail.py
@@ -1,31 +1,12 @@
 import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-
 username = 'tri.doan@sjsu.edu'
 password = '123456'
-
-
-def send_mail(text="Email Body", subject="Hello World", from_email="hello <tri.doan@sjsu.edu>", to_emails=None, html = None):
+def send_mail(text="Email Body", subject="Hello World", from_email  = "tri.doan@sjsu.edu" to_emails=[]):
     assert isinstance(to_emails, list)
-    msg = MIMEMultipart('alternative')
-    msg['From'] = from_email
-    msg['To'] = ",".join(to_emails)
-    msg['Subject'] = subject
-
-    txt_part = MIMEText(text, 'plain')
-    msg.attach(txt_part)
-
-    if html != None:
-        html_part = MIMEText('<h1> This is working</h1>', 'html')
-        msg.attach(html_part)
-
-    msg_str = msg.as_string()
-    server = smtplib.SMTP(host='smtp.gmail.com', port=587)
-
+    server = smtplib.SMTP()
     server.ehlo()
     server.starttls()
-    server.login(username, password)
-    server.sendmail(from_email, to_emails, msg_str)
+    server.login(username,password)
+    server.sendmail()
 
     server.quit()
