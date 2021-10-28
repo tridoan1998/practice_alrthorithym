7e51969b00f1fafec09f593302539e1babaf7b91
tridoan <tridoan1998@gmail.com>
2021-10-27 22:05:34 -0700

    new code Thu Oct 27

--- daily_coding_probkem.py
+++ daily_coding_probkem.py
@@ -8,3 +8,44 @@ class Solution:
                     if tempsum == 0:
                         arr.append([nums[i], nums[j], nums[k]])
         return arr
+
+
+"""
+
+For “ABDEFGABEF”, the longest substring are “BDEFGA” and “DEFGAB”, with length 6.
+For “BBBB” the longest substring is “B”, with length 1.
+For “GEEKSFORGEEKS”, there are two longest substrings shown in the below diagrams, with length 7
+
+
+
+time complexity: O(N)
+space complexity: O(1)
+data strucute: 
+technique:
+variable needed: 
+
+
+thought process:
+edge cases: all the same char => return length 1
+empty string, string with number, special character 
+
+
+
+
+"""
+
+
+class LinkedList:
+    class Node:
+        def __init__(self, data);
+
+        self.data = data
+
+
+def __init__(self):
+    self.head = None
+
+
+def longest_substring(head):
+
+
